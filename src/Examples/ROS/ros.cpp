f5795a55e38da05c8afdefe0d69a588f67a64935
DinossauroBebado <Guipsilva32@gmail.com>
2022-06-19 06:25:26 +0000

    add ros

--- src/Examples/ROS/ros.cpp
+++ src/Examples/ROS/ros.cpp
@@ -1,12 +1,30 @@
-#include <ros.h> 
-#include <std_msgs/Float64.h>
+/* 
+ * rosserial Subscriber Example
+ * Blinks an LED on callback
+ */
 
+#include <Main/config.h> 
+#include<Arduino.h>
+#include <ros.h>
+#include <std_msgs/Empty.h>
 
-void setup(){
+ros::NodeHandle  nh;
 
+void messageCb( const std_msgs::Empty& toggle_msg){
+  digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));   // blink the led
 }
 
-void loop(){
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
 
+void setup()
+{ 
+  pinMode(LED_BUILTIN, OUTPUT);
+  nh.initNode();
+  nh.subscribe(sub);
+}
 
+void loop()
+{  
+  nh.spinOnce();
+  delay(1);
 }
