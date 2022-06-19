2118a3dffb08f76f7c14da5bc667004fe97614e8
DinossauroBebado <Guipsilva32@gmail.com>
2022-06-19 06:28:23 +0000

    add

--- src/Examples/ROS/ros.cpp
+++ src/Examples/ROS/ros.cpp
@@ -11,14 +11,14 @@
 ros::NodeHandle  nh;
 
 void messageCb( const std_msgs::Empty& toggle_msg){
-  digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));   // blink the led
+  digitalWrite(LED_BUILT_IN, HIGH-digitalRead(LED_BUILT_IN));   // blink the led
 }
 
 ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
 
 void setup()
 { 
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILT_IN, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
 }
