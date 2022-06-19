bb01566003decf0bdd83e5e8715f9316fe6567e7
DinossauroBebado <guipsilva32@gmail.com>
2022-06-19 18:38:23 -0300

    .

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -37,45 +37,31 @@ void controlWheel( const std_msgs::Float32 &wheel_power,
     }
 
 void rightWheel(const std_msgs::Float32 &wheel_power){
-<<<<<<< HEAD
         controlWheel(wheel_power,CANAL_A,AIN1,AIN2);
-=======
->>>>>>> 47203d9743d31664a276171ae1ca382aaba4018a
         delay(2000);
         digitalWrite(LED_BUILD_IN,LOW);
 
 }
 
-<<<<<<< HEAD
+
 void leftWheel(const std_msgs::Float32 &wheel_power){
         controlWheel(wheel_power,CANAL_B,BIN1,BIN2);
-        delay(1000);
         digitalWrite(LED_BUILD_IN,LOW);
         delay(1000);
         digitalWrite(LED_BUILD_IN,HIGH);
         delay(1000);
         digitalWrite(LED_BUILD_IN,LOW);
-=======
+}
+
 void blink(const std_msgs::Float32 &wheel_power){
        for(int i =0;i<=wheel_power.data;i++){
-        digitalWrite(LED_BUILD_IN,HIGH);
-        delay(1000);
-        digitalWrite(LED_BUILD_IN,LOW);
-        delay(1000);
        }
->>>>>>> 47203d9743d31664a276171ae1ca382aaba4018a
-
-
 }
 
 ros::Subscriber<std_msgs::Float32> sub_right("wheel_power_right",&rightWheel);
-<<<<<<< HEAD
 ros::Subscriber<std_msgs::Float32> sub_left("wheel_power_left",&leftWheel);
 
 
-=======
-ros::Subscriber<std_msgs::Float32> sub_left("blink",&blink);
->>>>>>> 47203d9743d31664a276171ae1ca382aaba4018a
 void setup(){
   pinMode(PWMA, OUTPUT);//Definimos os pinos  como saída.
   pinMode(PWMB, OUTPUT);
